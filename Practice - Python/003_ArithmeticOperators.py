8187ea913c22742f4be1b4476b79bbfdd95168ca
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-03-16 04:26:36 -0500

    003

---
+++ Practice - Python/003_ArithmeticOperators.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+# # Tutorial:
+
+# #Python 2
+# a = int(raw_input())
+# b = int(raw_input())
+
+# #Python 3
+# a = int(input())
+# b = int(input())
